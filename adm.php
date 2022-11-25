173ae31e1ab8e69bb33859050bc0b2bff7c46123
Lapis <82874011+DiabolicWitch@users.noreply.github.com>
2022-11-25 09:17:57 -0300

    Working DELETE Button

--- adm.php
+++ adm.php
@@ -14,48 +14,47 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
   <!--NAVBAR-->
   <nav class="navbar navbar-custom navbar-expand-lg">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="index.html">Freddy Fazbear's Pizza</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse navbar" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="index.php">Home</a>
-              </li>
-              <?php
-                session_start();
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.html">Freddy Fazbear's Pizza</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse navbar" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="index.php">Home</a>
+          </li>
+          <?php
+          session_start();
 
-                if (isset($_SESSION['email'])) {
-              ?>
-                <li class="nav-item" style="float: right;">
-                  <a class="nav-link" href="logout.php">Log out</a>
-                </li>
-              <?php
-                if ($_SESSION['email'] == 'yuzuvulpes@gmail.com') {
-              ?>
-                <li class="nav-item">
-                  <a class="nav-link active" href="adm.php">Administrador</a>
-                </li>  
-              <?php             
-                }
-              }
-                else {
-              ?>
-                <li class="nav-item">
-                  <a class="nav-link" href="login.php">Login</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="register.php">Register</a>
-                </li>
-              <?php
-                }
-              ?>
-            </ul>
-          </div>
-        </div>
-      </nav>
+          if (isset($_SESSION['email'])) {
+          ?>
+            <li class="nav-item" style="float: right;">
+              <a class="nav-link" href="logout.php">Log out</a>
+            </li>
+            <?php
+            if ($_SESSION['email'] == 'yuzuvulpes@gmail.com') {
+            ?>
+              <li class="nav-item">
+                <a class="nav-link active" href="adm.php">Administrador</a>
+              </li>
+            <?php
+            }
+          } else {
+            ?>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="register.php">Register</a>
+            </li>
+          <?php
+          }
+          ?>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <!--END NAVBAR-->
 
   <!--Central Container-->
@@ -86,16 +85,26 @@
               </thead>
               <tbody>
                 <?php
-                  while($user = $res->fetch(PDO::FETCH_ASSOC)){
-                    $fid = $user['id'];
-                    echo "<tr>";
-                    echo "<td>".$user['id']."</td>";
-                    echo "<td>".$user['username']."</td>";
-                    echo "<td>".$user['email']."</td>";
-                    echo "<td>".$user['senha']."</td>";
-                    echo "<td><a type='button' class='btn btn-danger' href='deleteuser.php?$fid'>DELETE</a></td>";
-                    echo "</tr>";
-                 }
+                while ($user = $res->fetch(PDO::FETCH_ASSOC)) {
+                  echo "<tr>";
+                  echo "<td>" . $user['id'] . "</td>";
+                  echo "<td>" . $user['username'] . "</td>";
+                  echo "<td>" . $user['email'] . "</td>";
+                  echo "<td>" . $user['senha'] . "</td>";
+                  echo "<td><a type='button' class='btn btn-danger' href='adm.php?id=" . $user['id'] . "'>DELETE</a></td>";
+                  echo "</tr>";
+                }
+                ?>
+                <?php
+                require_once('./assets/config/connect.php');
+
+                if (isset($_GET['id'])) {
+                  $id = $_GET['id'];
+                  $stmt = $conn->prepare("DELETE FROM cadastros WHERE id = $id");
+                  if ($stmt->execute()) {
+                    echo 'user with ID ' . $id . ' was deleted successfully.';
+                  }
+                }
                 ?>
               </tbody>
             </table>
